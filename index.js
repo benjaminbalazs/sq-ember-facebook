01cb93b017dbcb9eb0c17226d1d2890c61c9564c
Benjamin Balazs <benjaminbalazs@gmail.com>
2016-01-15 22:04:49 +0400

    footer

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   name: 'sq-ember-facebook',
 
   contentFor: function(type, config) {
-      if ( type === 'head' ) {
+      if ( type === 'head-footer' ) {
         if ( config.FACEBOOK ) {
               if ( config.FACEBOOK.appId ) {
                   return "<script>(function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>";
