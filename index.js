baa0c2d4a2d13603e554badfad1bb8f4102531e3
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2016-08-30 12:25:34 +0400

    update

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ module.exports = {
       if ( type === 'head-footer' ) {
         if ( config.FACEBOOK ) {
               if ( config.FACEBOOK.appId ) {
-                  return "<script>(function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>";
+                  var meta = '<meta property="fb:app_id" content="'+config.FACEBOOK.appId+'" />';
+                  var script = "<script>(function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src='//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId="+config.FACEBOOK.appId+"';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>";
+                  return meta + script;
               }
           }
       }
+
   },
 
   isDevelopingAddon: function() {
